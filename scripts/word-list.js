8652305241009dcf277d5f1f6b1b53203605a792
edacapar <159358538+edacapar@users.noreply.github.com>
2024-02-08 00:08:43 +0300

    Add files via upload

---
+++ scripts/word-list.js
@@ -0,0 +1,74 @@
+const wordList = [
+    {
+        word: "elif",
+        hint: "Tuz ruhu kod adıyla bilinen tiktokerin ismi."
+    },
+    {
+        word: "kerimcan",
+        hint: "Eril oldugunda cok yakisikli olan unlu gay."
+    },
+    {
+        word: "zeybik",
+        hint: "Pangonun sevgilisinin adi."
+    },
+    {
+        word: "apollo",
+        hint: "Sikko sikko kombinler yapan tiktoker erkek olan."
+    },
+    {
+        word: "berivan",
+        hint: "fenomen dilenci yakuptvnin karisi."
+    },
+    {
+        word: "defne",
+        hint: "grwm ceken deli 09lu kiz."
+    },
+    {
+        word: "kazim",
+        hint: "populer bir dizide oynuyor bisey bisey papa."
+    },
+    {
+        word: "kelebek",
+        hint: "Seloyla olan ortak dövmemiz."
+    },
+    {
+        word: "eda",
+        hint: "Erva en cok kimi seviyor."
+    },
+    {
+        word: "matteo",
+        hint: "Tiktokta mavi kafali cocuk na na na na."
+    },
+    {
+        word: "berke",
+        hint: "en orospu meyveler vs. videolar ceken tiktokerin GERCEK ismi."
+    },
+    {
+        word: "sinem",
+        hint: "masa basi calisamam ele basi benim kizin ismi."
+    },
+    {
+        word: "sila",
+        hint: "Tiktokta hangisi daha guzel diye tartisilan kizlardan biri."
+    },
+    {
+        word: "mert",
+        hint: "Benim asik oldugum unlu erkek ask101 de oynuyo."
+    },
+    {
+        word: "gupse",
+        hint: "Omer iplikcinin gercekteki karisi."
+    },
+    {
+        word: "tilki",
+        hint: "Ozel gucleri olan cocuklar var eski bi dizide oynayan unlu ismi hayvan."
+    },
+    {
+        word: "kanepe",
+        hint: "Neyin kosesinde sabahliyorum."
+    },
+    {
+        word: "analiz",
+        hint: "Benim en nefret ettigim ders unide."
+    },     
+];
